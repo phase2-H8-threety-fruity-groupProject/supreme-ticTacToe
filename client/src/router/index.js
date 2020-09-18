c9dcd71798bf711df69191af7a740a62c18aed3b
rabakar <rifky.akbarmuhammad@gmail.com>
2020-09-18 09:42:21 +0700

    welcoming page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import WelcomePage from '../views/Welcome.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/welcome',
+    name: 'WelcomePage',
+    component: WelcomePage
   },
   {
     path: '/about',
