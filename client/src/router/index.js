71bf5140d99359eaaa94b3e70c0d454ab527aa66
rabakar <rifky.akbarmuhammad@gmail.com>
2020-09-18 10:11:23 +0700

    fix routing at welcome page to player registration to game

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,12 +7,12 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/gameOn',
     name: 'Home',
     component: Home
   },
   {
-    path: '/welcome',
+    path: '/',
     name: 'WelcomePage',
     component: WelcomePage
   },
